447b59080727fdb689f23d6bbf29cf867cf08431
pandeyrohan432-a11y <pandeyrohan432@gmail.com>
2026-09-16 10:35:54 +0530

    Disable browser spellcheck during mock

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import PairedHome from "./paired-home";
 
 export default function Home(){
-  return <><style dangerouslySetInnerHTML={{__html:`.card p{display:none!important}`}} /><PairedHome /></>;
+  return <>
+    <style dangerouslySetInnerHTML={{__html:`.card p{display:none!important}`}} />
+    <script dangerouslySetInnerHTML={{__html:`document.addEventListener('input',function(e){if(e.target&&e.target.tagName==='TEXTAREA')e.target.spellcheck=false;});document.querySelectorAll('textarea').forEach(function(e){e.spellcheck=false;});`}} />
+    <PairedHome />
+  </>;
 }
